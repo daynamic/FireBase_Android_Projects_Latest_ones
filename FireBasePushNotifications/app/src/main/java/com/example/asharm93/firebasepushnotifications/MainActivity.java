cea757cbd79a469d88b3b20fa92a081de9932c6c
Sharma <asharm93@LAMU02PG1VMG3QD.uhc.com>
2017-02-19 17:18:02 +0530

    Firebase Cloud Messaging Notifications Implementation

---
+++ FireBasePushNotifications/app/src/main/java/com/example/asharm93/firebasepushnotifications/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.asharm93.firebasepushnotifications;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ FireBasePushNotifications/app/src/main/java/com/example/asharm93/firebasepushnotifications/MyFirebaseInstanceIDService.java
@@ -0,0 +1,31 @@
+package com.example.asharm93.firebasepushnotifications;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by asharm93 on 2/19/17.
+ */
+//Class extending FirebaseInstanceIdService
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService
+{
+    private static final String TAG = "MyFirebaseIIDService";
+
+    @Override
+    public void onTokenRefresh() {
+
+        //Getting registration token
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        //Displaying token on logcat
+        Log.i(TAG, "Refreshed token: " + refreshedToken);
+
+    }
+
+    private void sendRegistrationToServer(String token) {
+        //You can implement this method to store the token on your server
+        //Not required for current project
+    }
+}
